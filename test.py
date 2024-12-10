e40f6a15719cf946cfbe13da2f7f4c0a3debe3c3
cherry1818 <129907637+cherry1818@users.noreply.github.com>
2024-12-10 12:21:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def test(model, test_loader, device):
     total = 0
     with torch.no_grad():  # Wyłączenie gradientów (niepotrzebne podczas testowania)
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)  # Przesłanie danych na GPU (jeśli dostępne)
+            data, target = data.to(device), target.to(device)  # Przesłanie danych na GPU
             output = model(data)
             pred = output.argmax(dim=1, keepdim=True) # Wybranie klasy z najwyższym prawdopodobieństwem
             correct += pred.eq(target.view_as(pred)).sum().item()
