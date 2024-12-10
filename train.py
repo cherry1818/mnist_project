cd564521e9ad8cb65764d4e1ddc0ab375cbaa237
cherry1818 <129907637+cherry1818@users.noreply.github.com>
2024-12-10 12:22:36 +0100

    Update train.py

--- train.py
+++ train.py
@@ -3,9 +3,9 @@ import torch.nn.functional as F
 
 def train(model, train_loader, optimizer, device):
     model.train()
-    total_loss = 0  # Inicjalizacja całkowitej straty
+    total_loss = 0  # Suma strat
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device) # Przesłanie danych na GPU (jeśli dostępne)
+        data, target = data.to(device), target.to(device) # Przesłanie danych na GPU 
         optimizer.zero_grad() # Zerowanie gradientów
         output = model(data)
         loss = F.cross_entropy(output, target) # Obliczenie straty (cross-entropy)
